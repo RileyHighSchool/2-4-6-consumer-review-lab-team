c034ee66ee3aa98b2bf0d41a81c12dac921fe599
DakotaNel <Dakota89577@students.sbcsc.k12.in.us>
2023-02-02 09:03:16 -0500

    a3

--- Main.java
+++ Main.java
@@ -1,12 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Review.sentimentVal("this"));
-        System.out.println(Review.sentimentVal("place"));
-        System.out.println(Review.sentimentVal("sucks"));
+        // System.out.println(Review.sentimentVal("this"));
+        // System.out.println(Review.sentimentVal("place"));
+        // System.out.println(Review.sentimentVal("sucks"));
         
 
-        System.out.println(Review.starRating("simpleReview.txt"));
-        System.out.println(Review.starRating("goodReview.txt"));
-        System.out.println(Review.starRating("badReview.txt"));
+        // System.out.println(Review.starRating("simpleReview.txt"));
+        // System.out.println(Review.starRating("goodReview.txt"));
+        // System.out.println(Review.starRating("badReview.txt"));
+
+        System.out.println(Review.fakeReview("simpleReview.txt"));
+
+        
     }
 }
